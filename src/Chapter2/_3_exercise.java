35cfe1fc66e6f351f8a06985e8d308efa9877d80
bozhidarkapudzhiev <bozhidar_kapudzhiev@abv.bg>
2023-07-17 12:37:41 +0300

    commit  17.07.2023

---
+++ src/Chapter2/_3_exercise.java
@@ -0,0 +1,10 @@
+package Chapter2;
+
+public class _3_exercise {
+    public static void main(String[] args) {
+        float floatPI=3.14159265389793238f;
+       double doublePI=3.14159265389793238;
+        System.out.println(floatPI);
+        System.out.println(doublePI);
+    }
+}
