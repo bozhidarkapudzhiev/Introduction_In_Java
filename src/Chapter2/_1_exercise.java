81fca3e02aef4a093ea82dc254f1e73177ed7499
bozhidarkapudzhiev <bozhidar_kapudzhiev@abv.bg>
2023-07-17 12:32:27 +0300

    commit  17.07.2023

---
+++ src/Chapter2/_1_exercise.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+public class _1_exercise {
+    public static void main(String[] args) {
+        byte centuries=20;
+        short years=2000;
+        int days=730480;
+        long hours=17531520;
+        System.out.println(centuries+" centuries is "+ years+" years,or  "+days+" days, or "+hours+ " hours."
+        );
+    }
+
+}
---
+++ src/Chapter2/_2_exercise.java
@@ -0,0 +1,10 @@
+package Chapter2;
+
+public class _2_exercise {
+    public static void main(String[] args) {
+        float floatPi=3.14f;
+        double doublePi=3.14;
+        System.out.println(floatPi);
+        System.out.println(doublePi);
+    }
+}
