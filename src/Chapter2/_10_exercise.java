519a73c8fe7327af3350ffea43d2d20b6588fe1a
bozhidarkapudzhiev <bozhidar_kapudzhiev@abv.bg>
2023-07-17 14:26:37 +0300

    commit  17.07.2023

---
+++ src/Chapter2/_10_exercise.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+public class _10_exercise {
+    public static void main(String[] args) {
+        int firstValue=5;
+        int secondValue;
+        int thridValue;
+        secondValue=firstValue;
+        thridValue=firstValue+3;
+        System.out.println(firstValue);
+        System.out.println(secondValue);
+        System.out.println(thridValue);
+    }
+}
---
+++ src/Chapter2/_9_exercise.java
@@ -0,0 +1,18 @@
+package Chapter2;
+
+public class _9_exercise {
+    public static void main(String[] args) {
+        byte centuries=20;
+        short years=2000;
+        int days=730480;
+        long hours=17531520;
+        float floatPI=3.14159265389793238f;
+        double doublePI=3.14159265389793238;;
+        boolean isEmpty=true;
+        char symbol='a';
+        String firstName="Ivan";
+
+        System.out.printf("%d%n%d%n%d%n%d%n%f%n%f%n%b%n%c%n%s%n",centuries,years,days,hours,floatPI,doublePI,isEmpty,symbol,firstName);
+
+    }
+}
