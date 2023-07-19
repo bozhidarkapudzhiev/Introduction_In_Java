ba4f07c6dc37e06d38c4e147db3e4cc42ce21965
bozhidarkapudzhiev <bozhidar_kapudzhiev@abv.bg>
2023-07-19 09:51:41 +0300

    19.07.2023

---
+++ src/Chapther3/_1_exercise.java
@@ -0,0 +1,12 @@
+package Chapther3;
+
+public class _1_exercise {
+    public static void main(String[] args) {
+        int z=8+4;
+        System.out.println(z);
+        String firstName="Lachezar";
+        String lastName="Bozhkov";
+        String fullName=firstName+" "+lastName;
+        System.out.println(fullName);
+    }
+}
